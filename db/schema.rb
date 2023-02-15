3b2e3f886cd5080650e54bca339b58da3ba65ee3
joao bernardo <joaogoncalob@gmail.com>
2023-02-15 00:21:09 +0000

    added the controller baskets and the model basket

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_220159) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_000220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "baskets", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "description"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cabazs", force: :cascade do |t|
     t.string "name"
